0d8870e35a4e1604481bffa25d0ad7233db80f64
IzMichael <michael@izmichael.com>
2023-07-30 00:07:42 +1200

    Fix DB link, missed during import from Loqui

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import { writable } from 'svelte/store';
 
-export const pb = new PocketBase('https://server.loqui.izmichael.com');
+export const pb = new PocketBase('https://db.traq.izmichael.com');
 pb.autoCancellation(false);
 
 export const currentUser = writable<User>(pb.authStore.model as unknown as User);
