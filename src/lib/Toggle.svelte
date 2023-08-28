3841259bd4a6399f8e2f5bdb255403104c4b9f61
IzMichael <michael@izmichael.com>
2023-08-28 21:11:32 +1200

    Add check marks to toggle component

--- src/lib/Toggle.svelte
+++ src/lib/Toggle.svelte
@@ -15,11 +15,13 @@
 </script>
 
 <label
-    class="aspect-square h-6 w-6 rounded-md border-2 border-blue-700 font-semibold transition-colors duration-200 ease-in-out {variants[
+    class="flex aspect-square h-6 w-6 flex-col items-center justify-center rounded-md border-2 border-blue-700 font-semibold transition-colors duration-200 ease-in-out {variants[
         variant
     ]} {xclass} {value ? 'bg-blue-500' : 'bg-transparent'}"
 >
     <input type="checkbox" bind:checked={value} class="invisible h-0 w-0 overflow-hidden" />
+
+    <img src="/assets/icons/check.svg" class="aspect-square h-full w-full -translate-y-0.5 invert" alt="Check Mark" />
 </label>
 
 <style>
